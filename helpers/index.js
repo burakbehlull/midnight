a12aaf50e91d7927ae880560e363d9f90462c558
burak behlül <burakyabgu@gmail.com>
2025-05-29 22:13:55 +0300

    feat: Add misc helper

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,7 @@
 import messageSender from "./messageSender.js"
+import * as misc from "./misc.js"
 
 export {
     messageSender,
+	misc
 }
---
+++ helpers/misc.js
@@ -0,0 +1,27 @@
+import { GatewayIntentBits } from 'discord.js';
+
+function randomColor(){
+	return Math.floor(Math.random() * (0xffffff + 1))
+}
+
+function itentsMiddle(){
+	return [
+		GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildVoiceStates,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMessageReactions,
+	]
+}
+
+function itentsAll(){
+    return Object.keys(GatewayIntentBits).map((intent) => GatewayIntentBits[intent])
+}
+
+export {
+	randomColor,
+	ItentsMiddle,
+	ItentsAll
+}
