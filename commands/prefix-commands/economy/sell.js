1cc9a56621e878b9ffd970aa761d4508f3c72826
burak behlül <burakyabgu@gmail.com>
2025-08-08 12:26:13 +0300

    fix: Fix sell command

--- commands/prefix-commands/economy/sell.js
+++ commands/prefix-commands/economy/sell.js
@@ -1,4 +1,4 @@
-import { Economy } from '#models';
+import { Economy, Shop } from '#models';
 import { messageSender } from '#helpers';
 
 export default {
@@ -11,16 +11,14 @@ export default {
     const sender = new messageSender(message);
     const userId = message.author.id;
     const [itemId, rawAmount] = args;
-	const shopItems = [
-	  { id: 1, name: 'Kalp', price: 200 },
-	  { id: 2, name: 'Gümüş Yüzük', price: 1000 },
-	  { id: 3, name: 'Altın Yüzük', price: 10000 },
-	  { id: 4, name: 'Elmas Yüzük', price: 100000 }
-	];
-    const item = shopItems.find(x => x.id.toString() === itemId);
+
     const amount = parseInt(rawAmount);
-    if (!item || isNaN(amount) || amount <= 0)
-      return sender.reply(sender.errorEmbed('❌ Geçerli item ve miktar gir.'));
+    if (!itemId || isNaN(amount) || amount <= 0)
+      return sender.reply(sender.errorEmbed('❌ Geçerli item ID ve miktar gir.'));
+
+    const item = await Shop.findOne({ id: parseInt(itemId) });
+    if (!item)
+      return sender.reply(sender.errorEmbed('❌ Bu ID ile bir item bulunamadı.'));
 
     const user = await Economy.findOne({ userId }) || new Economy({ userId });
     const currentAmount = user.inventory.get(itemId) || 0;
